ff8bdbbbf94fec971b326c2a82df2c3145d858e8
theroseknowz <110946128+theroseknowz@users.noreply.github.com>
2023-05-28 19:35:36 +0000

    Code passing all tests

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,50 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-
+  def __init__(self, **balls):
+    contents = []
+    self.contents = contents
+    for x in balls:
+      counter = 0
+      while counter < balls[x]:
+        contents.append(x)
+        counter += 1
+        
+  def draw(self,number):
+    counter = 0
+    drawnBalls = []
+    if number > len(self.contents):
+      drawnBalls = self.contents
+    else:
+      while counter < number:
+        drawnBall = random.choice(self.contents)
+        drawnBalls.append(drawnBall)
+        self.contents.remove(drawnBall)
+        counter +=1
+      if number > len(self.contents):
+        drawnBalls = self.contents
+    return drawnBalls 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  counter = 0
+  hits = 0
+  expectedContents = []
+  for x in expected_balls:
+    xcounter = 0
+    while xcounter < expected_balls[x]:
+      expectedContents.append(x)
+      xcounter += 1
+  while counter < num_experiments:
+    copyHat = copy.deepcopy(hat)
+    copyDrawn = copyHat.draw(num_balls_drawn)
+    reference = len(copyDrawn)
+    for x in expectedContents:
+      if x in copyDrawn:
+        copyDrawn.remove(x)
+      if len(copyDrawn) == (reference - len(expectedContents)):
+        hits += 1
+    counter += 1
+  return hits/num_experiments
+  
+
+        
--- test_module.py
+++ test_module.py
@@ -10,6 +10,9 @@ class UnitTests(unittest.TestCase):
         expected = ["red","red","red","blue","blue"]
         self.assertEqual(actual, expected, 'Expected creation of hat object to add correct contents.')
 
+
+
+  
     def test_hat_draw(self):
         hat = prob_calculator.Hat(red=5,blue=2)
         actual = hat.draw(2)
@@ -19,12 +22,22 @@ class UnitTests(unittest.TestCase):
         expected = 5
         self.assertEqual(actual, expected, 'Expected hat draw to reduce number of items in contents.')
 
+
+
+  
     def test_prob_experiment(self):
+
+
+      
         hat = prob_calculator.Hat(blue=3,red=2,green=6)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
         actual = probability
         expected = 0.272
         self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
+
+
+
+      
         hat = prob_calculator.Hat(yellow=5,red=1,green=3,blue=9,test=1)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
         actual = probability
